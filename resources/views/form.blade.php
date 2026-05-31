4f4e67804f127b5fe1e361ee9bdf42295b9ac752
[NotArato] <[pheng.an2016@gmail.com]>
2026-05-31 21:49:40 +0700

    Assignment Form Handling and Validation.

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -268,7 +268,7 @@
 
                         <form method="POST" action="/form/store">
                             @csrf
-
+ 
                             <div>
                                 <label for="name">Full name</label>
                                 <input id="name" name="name" type="text" value="{{ old('name') }}" placeholder="Your name" required>
